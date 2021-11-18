442b5240a1ab87dad02f95d67816b6c555eddbe0
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-18 00:16:36 -0500

    deleting file

--- modules/greet/components/HelloWorldGreet.tsx
+++
@@ -1,7 +0,0 @@
-import React, { VoidFunctionComponent } from 'react';
-
-const HelloWorldGreet: VoidFunctionComponent = () => {
-  return <h2>Hello World module!</h2>;
-};
-
-export default HelloWorldGreet;
