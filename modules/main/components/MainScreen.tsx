99ad3f1ca1615eb16e9fcdaabcf8fba3597ac36c
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-22 01:07:24 -0500

    using the editable component

--- modules/main/components/MainScreen.tsx
+++ modules/main/components/MainScreen.tsx
@@ -1,17 +1,23 @@
-import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
-import { isCompositeComponentWithType } from 'react-dom/test-utils';
+import clsx from 'clsx';
+import EditableText from 'components/EditableText';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  VoidFunctionComponent,
+} from 'react';
 import ReactPlayer from 'react-player';
 
-const MainScreen = () => {
-  const [player, setPlayer] = useState<any>();
+const MainScreen: VoidFunctionComponent = () => {
+  const [player, setPlayer] = useState<ReactPlayer>();
 
-  const ref = (playerParam: any) => {
+  const ref = (playerParam) => {
     setPlayer(playerParam);
-    // console.log('player', player);
   };
 
   const handleGetTime = () => {
     console.log('player.getCurrentTime()', player.getCurrentTime());
+    console.log('rows', rows);
   };
 
   // const editableRef = useRef<HTMLDivElement>(null);
@@ -23,54 +29,86 @@ const MainScreen = () => {
     return [...newArray];
   };
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLElement>, id) => {
-    console.log('ekey pres', e);
+  const [prevKey, setPrevKey] = useState('');
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>, id) => {
+    const row = rows.find((row) => row.id === id);
+    const indexRow = rows.indexOf(row);
+
+    const selection = window.getSelection();
+    if (!selection) return;
+    if (selection.rangeCount === 0) return;
+
+    const range = selection.getRangeAt(0);
+    const testRange = range.cloneRange();
+
+    const element = e.target as Node;
+    testRange.selectNodeContents(element);
+    testRange.setEnd(range.startContainer, range.startOffset);
+
+    const firstPart = testRange.toString().split('\n');
+
+    testRange.selectNodeContents(element);
+    testRange.setStart(range.endContainer, range.endOffset);
+
+    const lastPart = testRange.toString().split('\n');
+
+    const isFirstLine = firstPart.length === 1 && firstPart[0] === '';
+    const isLastLine = lastPart.length === 1 && lastPart[0] === '';
+
     if (e.key === 'Enter' && e.shiftKey === false) {
-      const row = rows.find((row) => row.id === id);
-
-      const indexRow = rows.indexOf(row);
-
       const newRows = addItemToArray(
-        rows,
+        [...rows],
         { value: '', id: indexRow + 1 },
-        indexRow,
-      );
-      setRows([...newRows.map((row, idx) => ({ ...row, id: idx }))]);
+        indexRow + 1,
+      ).map((row, idx) => ({ ...row, id: idx }));
+
+      setRows([...newRows]);
 
       setTimeout(() => {
-        // (editableRefs).forEach((editable, idx) => {
-        //   editable?.current?.focus();
-        // }
-        console.log('editableRefs', editableRefs);
         editableRefs?.current[indexRow + 1]?.focus();
+        console.log('rows', rows);
       }, 10);
-    } else if (e.code === 'Backspace') {
-      setRows((prevRows) => {
-        return [
-          ...rows.filter((row) => {
-            return row.id == id
-              ? (e.target as HTMLElement).innerText !== ''
-              : true; // & prevRows[idx] ? row.value !== '' : true;
-          }),
-        ];
-      });
+    } else if (prevKey == e.code && e.code === 'Backspace') {
+      setRows([
+        ...rows.filter((row) => {
+          return row.id == id
+            ? (e.target as HTMLDivElement).innerText.trim() !== ''
+            : true;
+        }),
+      ]);
       setTimeout(() => {
         editableRefs[id]?.current?.focus();
       }, 100);
+    } else if (prevKey == e.code && e.code === 'ArrowDown' && isLastLine) {
+      setTimeout(() => {
+        editableRefs?.current[indexRow + 1]?.focus();
+      }, 10);
+    } else if (prevKey == e.code && e.code === 'ArrowUp' && isFirstLine) {
+      setTimeout(() => {
+        editableRefs?.current[indexRow - 1]?.focus();
+      }, 10);
     } else {
+      console.log(
+        'e.target as HTMLDivElement).innerText',
+        (e.target as HTMLDivElement).innerText,
+      );
       setRows(
         rows.map((row) => {
           return row.id === id
-            ? { ...row, value: (e.target as HTMLElement).innerText }
+            ? { ...row, value: (e.target as HTMLDivElement).innerText }
             : { ...row };
         }),
       );
 
       // editableRef?.current?.focus();
-      // setTimeout(() => {
-      //   editableRef?.current?.focus();
-      // }, 100);
+      setTimeout(() => {
+        editableRefs?.current[indexRow]?.focus();
+      }, 100);
     }
+
+    setPrevKey(e.key);
+    console.log('rows', rows);
   };
 
   const [rows, setRows] = useState([
@@ -80,12 +118,14 @@ const MainScreen = () => {
     },
   ]);
 
-  return (
-    <div className="container">
-      <h1>Screen</h1>
+  // useEffect(() => {
+  //   editableRefs.current = [];
+  // }, [rows]);
 
-      <div className="grid grid-cols-2">
-        <div>
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="w-full overflow-auto">
           <ReactPlayer
             ref={ref}
             url="https://www.youtube.com/watch?v=6u-KAF5B9hc"
@@ -95,23 +135,20 @@ const MainScreen = () => {
             onPlay={() => console.log('onPlay')}
             onPause={() => console.log('onPause')}
             controls={true}
+            width="640px"
+            height="360px"
           />
         </div>
-        <div>
-          <div className="w-full p-4">
+        <div className="w-full">
+          <div className="w-full p-4 h-80 overflow-y-auto">
             {rows.map((row, idx) => (
               <div key={row.id} className="w-full">
-                <div
-                  className="w-full whitespace-pre-wrap break-words"
-                  contentEditable="true"
+                <EditableText
                   id={'input-' + row.id}
-                  // onChange={(e) => handleInputChange(e, row.id)}
-                  onKeyDown={(e) => handleKeyPress(e, row.id)}
-                  suppressContentEditableWarning={true}
-                  ref={(element) => (editableRefs.current[idx] = element)}
-                  placeholder="Enter text"
-                  data-ph="My Placeholder String"
-                ></div>
+                  handleKeyUp={(e) => handleKeyPress(e, row.id)}
+                  handleRef={(element) => (editableRefs.current[idx] = element)}
+                  placeholder={'Enter text'}
+                ></EditableText>
               </div>
             ))}
           </div>
@@ -120,7 +157,7 @@ const MainScreen = () => {
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
